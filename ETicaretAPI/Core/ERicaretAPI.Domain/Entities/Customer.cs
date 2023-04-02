8ccf4dec719d8714ed105abcc6562c29e7268883
Yasin Çoban <yasincoban1998@outlook.com>
2023-04-02 17:29:10 +0300

    database-migrate operations

---
+++ ETicaretAPI/Core/ERicaretAPI.Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using ERicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERicaretAPI.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string? Name { get; set; }     
+        public ICollection<Order>? Orders { get; set; }
+
+    }
+}
--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,5 +1,5 @@
-﻿using ETicaretAPI.Application.Abstractions;
-using ETicaretAPI.Persistence.Concretes;
+﻿using ETicaretAPI.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ETicaretAPI.Persistence
@@ -8,7 +8,10 @@ namespace ETicaretAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDbContext>(opt =>
+            {
+                opt.UseNpgsql(Configuration.ConnectionString);
+            });
 
         }
     }
